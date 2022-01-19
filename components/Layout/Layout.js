08f7440e1ea78559a0aa2fb2b59d4bdf32fe1ef3
Cristian Pérez Orozco <cristian.h.perez.o@gmail.com>
2022-01-19 16:59:42 +0100

    deleting unn

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -12,7 +12,6 @@ const Layout = props => {
             <title>NuweAbout</title>
             
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
-            <link href ="/static/css/app.css" rel ="stylesheet"/>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
             <link href="https://fonts.googleapis.com/css2?family=Jost:wght@100;300;600;900&display=swap" rel="stylesheet"/>
